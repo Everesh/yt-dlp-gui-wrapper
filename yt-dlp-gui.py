032fd64638a9db0e902db688ab607e43cf1514ff
Everesh <everesh@proton.me>
2025-03-20 23:20:00 +0100

    Add yt-dlp command creation

--- yt-dlp-gui.py
+++ yt-dlp-gui.py
@@ -28,16 +28,17 @@ class YTDLPGui:
         self.video_format = ttk.Combobox(self.root, values=["none", "best", "mp4", "webm", "avi", "mkv", "flv"], width=10)
         self.video_format.place(x=350, y=80)
         self.video_format.current(0)
+        self.video_format.bind("<<ComboboxSelected>>", lambda event: self.adjsut_audio_combobox())
 
         # PLaylist checkbox
-        tk.Label(self.root, text="Playlist:").place(x=480, y=80)
+        tk.Label(self.root, text="Playlist:").place(x=485, y=80)
         self.playlist = tk.BooleanVar()
         self.playlist.set(False)
-        tk.Checkbutton(self.root, variable=self.playlist).place(x=530, y=80)
+        tk.Checkbutton(self.root, variable=self.playlist).place(x=535, y=80)
 
         # Submit button
         self.submit_button = tk.Button(self.root, text="Submit", command=self.submit)
-        self.submit_button.place(x=580, y=75)
+        self.submit_button.place(x=605, y=75)
 
         # Status box
         self.status = scrolledtext.ScrolledText(self.root, wrap=tk.WORD, width=83, height=14)
@@ -48,8 +49,41 @@ class YTDLPGui:
     def submit(self):
         """Handles the submit button click."""
         url = self.url.get()
+        self.url.delete(0, tk.END)
+
         audio_format = self.audio_format.get()
-        self.status.insert(tk.END, f"Downloading: {url} in {audio_format} format\n")
+        video_format = self.video_format.get()
+        playlist = self.playlist.get()
+        self.log(f"==> Downloading: {url}\n==> Video: {video_format}\n==> Audio: {audio_format}\n==> Playlist: {playlist}\n")
+
+        command = "yt-dlp -f" # Starts building the yt-dlp command
+
+        if video_format != "none":
+            command += " bestvideo+bestaudio"
+            if video_format != "best":
+                command += f" --recode-video {video_format}"
+        else:
+            command += " bestaudio"
+            if audio_format != "best":
+                command += f" --extract-audio --audio-format {audio_format}"
+
+        if not playlist:
+            command += " --no-playlist"
+
+        command += f" {url}"
+
+        self.log(command)
+
+    def adjsut_audio_combobox(self):
+        """Grays out the audio combobox if the video format is not none."""
+        if self.video_format.get() == "none":
+            self.audio_format["state"] = "normal"
+        else:
+            self.audio_format["state"] = "disabled"
+
+    def log(self, message):
+        """Logs a message to the status box."""
+        self.status.insert(tk.END, message)
         self.status.see(tk.END)
 
 if sys.platform == "win32":
